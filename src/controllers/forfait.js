6aa6d2c127067ad7f69527bdf80bd72f2fd80ae7
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-05-14 12:28:38 +0300

    fix validation

--- src/controllers/forfait.js
+++ src/controllers/forfait.js
@@ -1,14 +1,17 @@
 const { Sequelize } = require("../config/database");
 const forfait = require("./../models/forfait");
-
+const { param, validationResult } = require("express-validator");
 const Router = require("express").Router();
 
-Router.get(async (req, res) => {
+Router.get("/", async (req, res) => {
   return res.json(await forfait.findAll());
 });
 
-Router.get(async (Req, res) => {
-
+Router.get("/:id", param("id").isNumeric(), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   return res.json(await forfait.findOrFail({}));
 });
 
