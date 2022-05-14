d4819bd1383fd0a795785b2088050260697347be
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-05-14 13:21:09 +0300

    fix update forfait

--- src/controllers/forfait.js
+++ src/controllers/forfait.js
@@ -12,7 +12,7 @@ Router.get("/:id", param("id").isNumeric().notEmpty(), async (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  return res.json(await forfait.findOrFail({}));
+  return res.json(await forfait.findOne({ where: { id: req.params.id } }));
 });
 Router.post(
   "/",
@@ -30,6 +30,32 @@ Router.post(
 
     forfait
       .create({
+        ...req.body,
+        typeData: req.body.typeData.toString(),
+        emplacement: req.body.emplacement.toString(),
+      })
+      .then((forfait) => res.json(forfait));
+  }
+);
+
+Router.put(
+  "/:id",
+  param("id").isNumeric().notEmpty(),
+  body("forfait_name").isString().isLength({ min: 4 }).notEmpty(),
+  body("typeData").isArray().notEmpty(),
+  body("tailleMax").isString().notEmpty().toInt(),
+  body("durationMax").isString().notEmpty().toInt(),
+  body("emplacement").isArray().notEmpty(),
+  body("prix").isString().notEmpty().toInt(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    forfait
+      .upsert({
+        id: req.params.id,
         ...req.body,
         typeData: req.body.typeData.toString(),
         emplacement: req.body.emplacement.toString(),
